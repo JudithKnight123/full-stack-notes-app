2302897b485846398cc1a44f31a0969d5a303cd9
JudithKnight123 <judithknight.roles@gmail.com>
2026-07-10 12:12:58 +0100

    restructure

--- public/script.js
+++ public/script.js
@@ -1,46 +1,13 @@
-// 1. use document.getElementById to select the searchTerm Button
-var searchInput = document.getElementById("saveTerm");
-// 2. use document.getElementById to select the searchButton Button
-var saveButton = document.getElementById("saveButton");
-// 3. add an event listener to the searchButton that calls the search function when clicked
-saveButton.addEventListener("click", onClickSaveButton);
-
-function onClickSaveButton() {
-  // 4. use the value property of the searchInput to get the search term
-  //TODO:
-  var saveTerm = searchInput.value;
-  
-  if (saveTerm === "") {
-  alert("Please enter a task before adding it.");
-  return;
-}
-  // 5. select the searches div using document.getElementById
-  var searchesDiv = document.getElementById("searches");
-  //TODO:
-  // 6. create a new li element using document.createElement
-  var newLi = document.createElement("li");
-  //TODO:
-  // 7. set the innerHTML of the new paragraph to the search term
-newLi.innerHTML = searchInput.value;
-  //TODO:
-  // 8. append the new paragraph to the searches div
-searchesDiv.appendChild(newLi);
-  //TODO:
-  // 9. Button click event - create a delete button
-var clickButton = document.createElement("button");
-clickButton.innerHTML = "Delete";
-clickButton.addEventListener("click", function () {
-  newLi.remove();
-});
-newLi.appendChild(clickButton); //put Button inside li
-
-}
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const dataList = document.getElementById("searches");
   const dataInput = document.getElementById("saveTerm");
   const saveButton = document.getElementById("saveButton");
+
+    if (saveTerm === "") {
+  alert("Please enter a task before adding it.");
+  return;
+}
+
 
   // Function to fetch data from the backend
   const fetchData = async () => {
@@ -50,8 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
       dataList.innerHTML = ""; // Clear the list before rendering
       data.forEach((item) => {
         const li = document.createElement("li");
+
+        // Button click event - create a delete button
+        const clickButton = document.createElement("button");
+        clickButton.innerHTML = "Delete";
+        
+        clickButton.addEventListener("click", async function () {
+            const response = await fetch("/data/" + item.id, {
+                method: "DELETE"
+             });
+            li.remove();
+        });
+
         li.textContent = item.message;
-        // li.textContent = item.id + ": " + JSON.stringify(item);
+        
+        li.appendChild(clickButton);//put Button inside li
         dataList.appendChild(li);
       });
     } catch (error) {
